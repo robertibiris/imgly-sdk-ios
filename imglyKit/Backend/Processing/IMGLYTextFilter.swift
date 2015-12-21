8c133269c039aa33094e21b43ce8b474e9419586
Sascha Schwabbauer <sascha.schwabbauer@9elements.com>
2015-12-21 18:40:37 +0100

    Edit filters to use protocols

--- imglyKit/Backend/Processing/IMGLYTextFilter.swift
+++ imglyKit/Backend/Processing/IMGLYTextFilter.swift
@@ -14,7 +14,7 @@ import QuartzCore
 import AppKit
 #endif
 
-public class IMGLYTextFilter: CIFilter {
+public class IMGLYTextFilter: CIFilter, FilterType {
     /// A CIImage object that serves as input for the filter.
     public var inputImage: CIImage?
     /// The text that should be rendered.
